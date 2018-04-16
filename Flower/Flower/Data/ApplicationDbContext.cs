816814213e34a645bfbfde35e7d966d029ad0e38
Patryk Matuszak <matuszak.patrykk@gmail.com>
2018-04-16 17:23:03 +0200

    temporary commit

--- Flower/Flower/Data/ApplicationDbContext.cs
+++ Flower/Flower/Data/ApplicationDbContext.cs
@@ -18,9 +18,6 @@ namespace Flower.Data
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
-            // Customize the ASP.NET Identity model and override the defaults if needed.
-            // For example, you can rename the ASP.NET Identity table names and more.
-            // Add your customizations after calling base.OnModelCreating(builder);
         }
 
         public DbSet<Flower.Models.Customer> Customer { get; set; }
---
+++ Flower/Flower/Data/Repository/ProductRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Flower.Data.Repository.Interfaces;
+using Flower.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Flower.Data.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public ProductRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void Add(Product product)
+        {
+            _dbContext.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+            _dbContext.Remove(product);
+        }
+
+        public async Task<IEnumerable<Product>> GetAll()
+        {
+            return await _dbContext.Product.ToListAsync();
+        }
+
+        public async Task<Product> GetById(string Id)
+        {
+            return await _dbContext.Product.FirstOrDefaultAsync(x => x.ProductID == Int64.Parse(Id));
+        }
+
+        public void Update(Product product)
+        {
+            _dbContext.Update(product);
+        }
+    }
+}
