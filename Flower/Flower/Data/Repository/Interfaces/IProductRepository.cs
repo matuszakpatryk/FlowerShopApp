ba62b37edb2baa6b2374a614e48a1e5e99d74551
Patryk Matuszak <matuszak.patrykk@gmail.com>
2018-04-17 09:13:36 +0200

    add Products repository

---
+++ Flower/Flower/Data/Repository/Interfaces/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Flower.Models;
+
+namespace Flower.Data.Repository.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetAll();
+        Task<Product> GetById(string Id);
+        void Add(Product product);
+        void Update(Product product);
+        void Delete(Product product);
+    }
+}
